26dded7e0a855dc8d8b80e93a4562ede6f4e3c98
Ben Hutchings <ben@decadent.org.uk>
2023-01-13 23:56:47 +0100

    [klibc] select: Remove support for pselect7() syscall
    pselect7() doesn't seem to have been implemented by any architecture
    in the upstream kernel.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- usr/klibc/pselect.c
+++ usr/klibc/pselect.c
@@ -5,20 +5,6 @@
 #include <sys/select.h>
 #include <sys/syscall.h>
 
-#if defined(__NR_pselect7)
-
-__extern int __pselect7(int, fd_set *, fd_set *, fd_set *,
-			const struct timespec *, const sigset_t *, size_t);
-
-int pselect(int n, fd_set * readfds, fd_set * writefds, fd_set * exceptfds,
-	    const struct timespec *timeout, const sigset_t * sigmask)
-{
-	return __pselect7(n, readfds, writefds, exceptfds,
-			  timeout, sigmask, sizeof *sigmask);
-}
-
-#elif defined(__NR_pselect6)
-
 struct __pselect6 {
 	const sigset_t *sigmask;
 	size_t sigsize;
@@ -34,5 +20,3 @@ int pselect(int n, fd_set * readfds, fd_set * writefds, fd_set * exceptfds,
 	return __pselect6(n, readfds, writefds, exceptfds,
 			  timeout, &extended_sigmask);
 }
-
-#endif
