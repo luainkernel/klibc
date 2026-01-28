60e3aadc7cd330bd02b1236d9bce3baff2571594
Marcel Moura <5615598+marcelstanley@users.noreply.github.com>
2026-01-28 15:35:33 -0300

    arm: make divmoddi4 compatible

--- usr/klibc/arch/arm/__divmoddi4.c
+++ usr/klibc/arch/arm/__divmoddi4.c
@@ -10,19 +10,22 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "int_lib.h"
+#include <stdint.h>
+#include <limits.h>
 
 // Returns: a / b, *rem = a % b
 
-COMPILER_RT_ABI di_int __divmoddi4(di_int a, di_int b, di_int *rem) {
-  const int bits_in_dword_m1 = (int)(sizeof(di_int) * CHAR_BIT) - 1;
-  di_int s_a = a >> bits_in_dword_m1;                   // s_a = a < 0 ? -1 : 0
-  di_int s_b = b >> bits_in_dword_m1;                   // s_b = b < 0 ? -1 : 0
-  a = (du_int)(a ^ s_a) - s_a;                          // negate if s_a == -1
-  b = (du_int)(b ^ s_b) - s_b;                          // negate if s_b == -1
-  s_b ^= s_a;                                           // sign of quotient
-  du_int r;
-  di_int q = (__udivmoddi4(a, b, &r) ^ s_b) - s_b;      // negate if s_b == -1
-  *rem = (r ^ s_a) - s_a;                               // negate if s_a == -1
-  return q;
+extern uint64_t __udivmoddi4(uint64_t num, uint64_t den, uint64_t * rem);
+
+int64_t __divmoddi4(int64_t a, int64_t b, int64_t *rem) {
+	  const int bits_in_dword_m1 = (int)(sizeof(int64_t) * CHAR_BIT) - 1;
+	  int64_t s_a = a >> bits_in_dword_m1;                   // s_a = a < 0 ? -1 : 0
+	  int64_t s_b = b >> bits_in_dword_m1;                   // s_b = b < 0 ? -1 : 0
+	  a = (uint64_t)(a ^ s_a) - s_a;                          // negate if s_a == -1
+	  b = (uint64_t)(b ^ s_b) - s_b;                          // negate if s_b == -1
+	  s_b ^= s_a;                                           // sign of quotient
+	  uint64_t r;
+	  int64_t q = (__udivmoddi4(a, b, &r) ^ s_b) - s_b;      // negate if s_b == -1
+	  *rem = (r ^ s_a) - s_a;                               // negate if s_a == -1
+	  return q;
 }
